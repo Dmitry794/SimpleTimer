23ddb4d7aac50f655ab5ff04a22fd2a23d48da90
Dmitry79488 <karpesh.ds.86@mail.ru>
2016-10-08 20:27:58 +0300

    first commit: add start log

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("Start");
         TimeReporter.startCounter("processor");
         proc = new ArrayList<>();
         proc.add(new Command(2240));
